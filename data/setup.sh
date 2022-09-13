b1198f2a56c59f4588d5ccbed1f095eb95e09585
poseidon-code <61552390+poseidon-code@users.noreply.github.com>
2022-09-13 10:00:00 +0530

    update: modular cleanup

--- data/setup.sh
+++ data/setup.sh
@@ -6,18 +6,21 @@ wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Train_mscoco.zip
 unzip v2_Questions_Train_mscoco.zip -d feats/train2014
 wget http://images.cocodataset.org/zips/train2014.zip
 unzip train2014.zip -d feats/train2014
+rm -rvf train2014.zip v2_Questions_Train_mscoco.zip
 
 # download VQA 2017 validation dataset
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Val_mscoco.zip
 unzip v2_Questions_Val_mscoco.zip -d feats/val2014
 wget http://images.cocodataset.org/zips/val2014.zip
 unzip val2014.zip -d feats/val2014
+rm -rvf val2014.zip v2_Questions_Val_mscoco.zip
 
 # download VQA 2017 testing dataset
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Questions_Test_mscoco.zip
 unzip v2_Questions_Test_mscoco.zip -d feats/test2015
 wget http://images.cocodataset.org/zips/test2015.zip
 unzip test2015.zip -d feats/test2015
+rm -rvf test2015.zip v2_Questions_Test_mscoco.zip
 
 
 
@@ -29,12 +32,9 @@ cp feats/test2015/v2_OpenEnded_mscoco_test2015_questions.json raw/
 # download VQA 2017 training annotations
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Train_mscoco.zip
 unzip v2_Annotations_Train_mscoco.zip -d raw/
+rm -rvf v2_Annotations_Train_mscoco.zip
 
 # download VQA 2017 validation annotations
 wget https://s3.amazonaws.com/cvmlp/vqa/mscoco/vqa/v2_Annotations_Val_mscoco.zip
 unzip v2_Annotations_Val_mscoco.zip -d raw/
-
-
-
-# cleanup
-rm -rvf v2_Annotations_Train_mscoco.zip v2_Annotations_Val_mscoco.zip v2_Questions_Train_mscoco.zip v2_Questions_Val_mscoco.zip v2_Questions_Test_mscoco.zip train2014.zip val2014.zip test2015.zip
+rm -rvf v2_Annotations_Val_mscoco.zip
