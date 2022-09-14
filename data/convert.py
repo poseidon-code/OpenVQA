e868eb4bdeb07acbd83c6c02736452493e9b7e52
poseidon-code <61552390+poseidon-code@users.noreply.github.com>
2022-09-14 23:15:14 +0530

    update: parallel npz conversion

--- data/convert.py
+++ data/convert.py
@@ -1,16 +1,34 @@
 import glob
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 import numpy as np
 from PIL import Image
 
 
-def JPGtoNPZ(configuration):
+COUNT = 0
+def imageToNPZ(configuration, type):
+    print(f':: Converting {type.upper()} to NPZ files')
+    def convert(path):
+        global COUNT
+        image = Image.open(path)
+        imageArray = np.array(image)
+        np.savez_compressed(f'{path[:-4]}.npz', x=imageArray)
+        os.remove(path)
+        COUNT+=1
+
     for dataType in configuration.FEATS_PATH:
-        imagePaths = glob.glob(f'{configuration.FEATS_PATH[dataType]}/*.jpg')
-        for path in imagePaths:
-            imageName = os.path.basename(path)
-            image = Image.open(path)
-            imageArray = np.array(image)
-            np.savez_compressed(f'{path[:-4]}.npz', x=imageArray)
-            os.remove(path)
+        global COUNT
+        COUNT = 0
+        imagePaths = glob.glob(f'{configuration.FEATS_PATH[dataType]}/*.{type}')
+        total = len(imagePaths)
+
+        if total == 0:
+            print(f'!! No {type.upper()} images found for {dataType}')
+        else:
+            with ThreadPoolExecutor(max_workers=10) as e:
+                for _ in e.map(convert, imagePaths):
+                    print(f'>> {dataType} : {COUNT}/{total}', end='\r')
+            print()
+
+    print(':: Finished Conversion')
